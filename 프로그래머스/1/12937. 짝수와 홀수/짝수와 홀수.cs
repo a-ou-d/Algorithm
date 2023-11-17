79bb66f1b3ad2e936695ffcf817662a11da540b9
a-ou-d <148048020+a-ou-d@users.noreply.github.com>
2023-11-17 14:53:40 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.16 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.cs
@@ -0,0 +1,18 @@
+public class Solution
+{
+    public string solution(int num)
+    {
+        if (num % 2 == 0)
+        {
+            return("Even");
+        }
+        else if (num == 0)
+        {
+            return("Even");
+        }
+        else
+        {
+            return("Odd");
+        }
+    }
+}
